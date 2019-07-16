76fd406c33f3d301535d4991a9e86dc5ff7ab977
Aiolos <antfinaiolos@IT-C02V266KG8WN.local>
2019-07-16 11:44:11 +0800

    a little little change

--- workspace-gateway/src/main/java/com/aliware/tianchi/CallbackListenerImpl.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/CallbackListenerImpl.java
@@ -14,22 +14,32 @@ public class CallbackListenerImpl implements CallbackListener {
     static long memory_large = 3;
     static long memory_medium = 2;
     static long memory_small = 1;
+    static short mask = 0;
 
     @Override
     public void receiveServerMsg(String msg) {
         //System.out.println("receive msg from server :" + msg);
         String[] rcvmsglist = msg.split(",");
-        if (rcvmsglist[0].equals("small")) memory_small = Long.parseLong(rcvmsglist[1]);
-        else if (rcvmsglist[0].equals("medium")) memory_medium = Long.parseLong(rcvmsglist[1]);
-        else memory_large = Long.parseLong(rcvmsglist[1]);
+        if (rcvmsglist[0].equals("small")) {
+            memory_small = Long.parseLong(rcvmsglist[1]);
+            mask |= 0x001;
+        }
+        else if (rcvmsglist[0].equals("medium")) {
+            memory_medium = Long.parseLong(rcvmsglist[1]);
+            mask |= 0x010;
+        }
+        else {
+            memory_large = Long.parseLong(rcvmsglist[1]);
+            mask |= 0x100;
+        }
 
-        long memory_sum = memory_large + memory_medium + memory_small;
-
-        //System.out.println(msg + " : " + memory_large + ',' + memory_medium + ',' + memory_small + "  |  " + memory_sum);
-
-        UserLoadBalance.weight_large = 6.0 * memory_large / memory_sum;
-        UserLoadBalance.weight_medium = 6.0 * memory_medium / memory_sum;
-        UserLoadBalance.weight_small = 6.0 * memory_small / memory_sum;
+        if (mask == 0x111) {
+            long memory_sum = memory_large + memory_medium + memory_small;
+            //System.out.println(msg + " : " + memory_large + ',' + memory_medium + ',' + memory_small + "  |  " + memory_sum);
+            UserLoadBalance.weight_large = 6.0 * memory_large / memory_sum;
+            UserLoadBalance.weight_medium = 6.0 * memory_medium / memory_sum;
+            UserLoadBalance.weight_small = 6.0 * memory_small / memory_sum;
+        }
     }
 
     private String cachedServerCpu;
