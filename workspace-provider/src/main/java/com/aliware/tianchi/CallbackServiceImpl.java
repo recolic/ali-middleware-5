e54247f370dc0895c9ad5936aa50a62d484ed6c4
Aiolos <antfinaiolos@IT-C02V266KG8WN.local>
2019-07-16 11:51:44 +0800

    day2 start version

--- workspace-provider/src/main/java/com/aliware/tianchi/CallbackServiceImpl.java
+++ workspace-provider/src/main/java/com/aliware/tianchi/CallbackServiceImpl.java
@@ -37,7 +37,7 @@ public class CallbackServiceImpl implements CallbackService {
                     }
                 }
             }
-        }, 0, 1000);
+        }, 0, 2000);
     }
 
     private Timer timer = new Timer();
